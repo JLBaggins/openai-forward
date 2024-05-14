d6c76d12204df98a9700d5f355c1d9743e440a5e
Jacob Speer <jacoblspeer@gmail.com>
2024-05-14 09:55:41 -0400

    Update base.py

--- openai_forward/base.py
+++ openai_forward/base.py
@@ -127,7 +127,7 @@ class OpenaiBase:
             content=request.stream(),
             timeout=cls.timeout,
         )
-        print(req)
+        print(req.content)
         try:
             r = await client.send(req, stream=True)
         except (httpx.ConnectError, httpx.ConnectTimeout) as e:
